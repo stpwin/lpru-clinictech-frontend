ac2256bf91a4ccccdde4d5f8c29bc30513d68fa8
stpwin <isysforce@gmail.com>
2020-10-10 02:22:45 +0700

    remove unuse database code

--- src/firebaseApp.js
+++ src/firebaseApp.js
@@ -6,7 +6,7 @@ import { firebaseConfig } from "./firebaseConfig";
 import { ReactIsInDevelomentMode } from "./utils";
 
 firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
+// const database = firebase.database();
 const functions = firebase.functions();
 // const auth = firebase.auth();
 
@@ -24,45 +24,4 @@ export const sendAsk = async (name, email, content) => {
     IsDeveloment ? "sendMessageDev" : "sendMessage"
   );
   return sendMessage({ name, email, content });
-  // return auth.signInAnonymously().then(async (user) => {
-  //   await auth.currentUser.updateProfile({ displayName: name });
-  //   await auth.currentUser.updateEmail(email);
-  //   // console.log({ user });
-  //   const sendMessage = functions.httpsCallable(
-  //     IsDeveloment ? "sendMessageDev" : "sendMessage"
-  //   );
-  //   return sendMessage({ name, email, content });
-  // });
-};
-
-export const getWebContact = () => {
-  const webInfoCache = localStorage.getItem("webInfo");
-  if (webInfoCache) {
-    try {
-      return Promise.resolve(JSON.parse(webInfoCache));
-    } catch {
-      localStorage.setItem("webInfo", null);
-    }
-  }
-  const webContactDB = database.ref("web-contact");
-  return webContactDB
-    .once("value")
-    .then((snapshot) => {
-      const { email, facebook, fax, place, tel, footerText } = snapshot.val();
-      localStorage.setItem(
-        "webInfo",
-        JSON.stringify({
-          email,
-          facebook,
-          fax,
-          place,
-          tel,
-          footerText
-        })
-      );
-      return { email, facebook, fax, place, tel, footerText };
-    })
-    .catch((err) => {
-      console.warn(err);
-    });
 };
